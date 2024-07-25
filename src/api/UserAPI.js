0668473849c70be96d03ddab07900b11e2bc3c46
Dev Mittal <mittal.dev2002@gmail.com>
2024-07-25 13:26:20 +0530

    changing frontend

--- src/api/UserAPI.js
+++ src/api/UserAPI.js
@@ -11,7 +11,7 @@ function UserAPI(token) {
         if(token){
             const getUser = async () =>{
                 try {
-                    const res = await axios.get('/user/infor', {
+                    const res = await axios.get('https://ecom-backend-qkpn.onrender.com/user/infor', {
                         headers: {Authorization: token}
                     })
 
@@ -42,7 +42,7 @@ function UserAPI(token) {
         if(check){
             setCart([...cart, {...product, quantity: 1}])
 
-            await axios.patch('/user/addcart', {cart: [...cart, {...product, quantity: 1}]}, {
+            await axios.patch('https://ecom-backend-qkpn.onrender.com/user/addcart', {cart: [...cart, {...product, quantity: 1}]}, {
                 headers: {Authorization: token}
             })
 
